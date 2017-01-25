bcfc187f3d9d9cf2219a7e378d963d17b54cd9c0
Alexander Taxter <Sharquo@gmail.com>
2017-01-25 16:21:25 +0000

    Added several controller scripts.  Need to finsh roidController.

---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+
+    public float ttd = 1.0f;
+    public float pew = 400.0f;
+
+	void Start ()
+    {
+        // Set the laser to destroy itself after ttd.
+        Destroy(gameObject, ttd);
+
+        // Push the laser in the direction that it is facing.
+        GetComponent<Rigidbody2D>().AddForce(transform.up * pew);
+	}
+	
+}
---
+++ Assets/Scripts/roidController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class roidController : MonoBehaviour
+{
+
+    public AudioClip destroy;
+    public GameObject roidSmall;
+
+    private GameController gameController;
+    
+	void Start ()
+    {
+        // Get a ereference to the game controller object and script.
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+
+        gameController = gameControllerObject.GetComponent<GameController>();
+
+        // Push the asteroid in the direction it is facing.
+        GetComponent<Rigidbody2D>().AddForce(transform.up * Random.Range(-50.0f, 150f));
+
+        // Give a randomg angular velocity.
+        GetComponent<Rigidbody2D>().angularVelocity = Random.Range(-0.0f, 90.0f);
+	}
+	
+
+}
